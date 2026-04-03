34551d2e740fdb5ee546406d62fa3ad9d6cd3f3d
thiagopinheeir-tech <thiagopinheeir@gmail.com>
2026-04-02 22:39:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from 'virtual:pwa-register';
+
+// Registro do Service Worker com auto-update
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(<App />);
